bba8262a3f67bc07f1624f3fec414e73be01e769
DouglasGuerra <douglas.flurucava@hotmail.com>
2023-11-07 22:56:43 -0300

    Form pro create 100%

--- wp-content/themes/agendamentoabc/forms/form-pro.php
+++ wp-content/themes/agendamentoabc/forms/form-pro.php
@@ -4,49 +4,54 @@
         <form class="row g-3">
         <div class="col-md-6">
             <label for="inputEmail4" class="form-label">Nome Completo</label>
-            <input type="email" class="form-control" id="inputEmail4" required="required" name="name_patient">
+            <input type="email" class="form-control" id="inputEmail4" required="required" name="name_pro">
         </div>
         <div class="col-md-6">
             <label for="inputPassword4" class="form-label">Email</label>
-            <input type="email" class="form-control" id="inputPassword4" required="required" name="email_patient">
+            <input type="email" class="form-control" id="inputPassword4" required="required" name="email_pro">
         </div>
-        <div class="col-6">
-            <label for="inputAddress" class="form-label">Horário da consulta</label>
-            <select class="form-select" aria-label="Default select example" name="hour_patient">
-            <option disabled>Escolha uma horário para atendimento</option>
-            <option value="1">8:00 - 8:50</option>
-            <option value="2">9:00 = 9:50</option>
-            <option value="1">10:00 - 8:50</option>
-            <option value="2">11:00 = 9:50</option>
-            <option value="1">12:00 - 8:50</option>
-            <option value="2">13:00 = 9:50</option>
-            <option value="1">14:00 - 8:50</option>
-            <option value="2">15:00 = 9:50</option>
-            <option value="2">16:00 = 9:50</option>
-            <option value="2">17:00 = 9:50</option>
-            
-            </select>
+  
+        <div class="col-4">
+            <label for="inputAddress2" class="form-label">CRP</label>
+            <input type="tel" class="form-control" id="inputAddress2" required="required"  name="crp_pro">
         </div>
-        <div class="col-6">
-            <label for="inputAddress2" class="form-label">Whatsapp</label>
-            <input type="tel" class="form-control" id="inputAddress2" required="required placeholder="Digite seu numero" name="number_patient">
+        <div class="col-4">
+            <label for="inputAddress" class="form-label">Especialização</label>
+            <select class="form-select" aria-label="Default select example" name="specialization_pro" required="required">
+            <option disabled>Especialização</option>
+            <option value="1">Adulto</option>
+            <option value="2">Juvenil</option>
+            <option value="3">Casal</option>
+            </select>       
         </div>
-        <div class="col-2">
-            <label for="inputAddress" class="form-label">Horário da consulta</label>
-            <select class="form-select" aria-label="Default select example" name="hour_patient" required="required>
-            <option disabled">Estado</option>
-            <option value="1">RJ</option>
-            <option value="2">SP</option>
-            <option value="2">ES</option>
-            <option value="2">DF</option>
-            <option value="2">MG</option>
-            <option value="2">PR</option>
-            <option value="2">Outros</option>    
-            </select>
+        <div class="col-4">
+            <label for="inputAddress" class="form-label">Demanda de preferência</label>
+            <select class="form-select" aria-label="Default select example" name="specialization_pro" required="required">   
+            <option value="4">Autoestima</option>
+            <option value="5">Ansiedade</option>
+            <option value="6">Depressão</option>
+            <option value="7">Fobias e Medos</option>
+            <option value="8">Relacionamentos</option>    
+            <option value="9">Terapia de Casal</option>
+            <option value="10">Terapia para Adolescente</option>
+            <option value="11">Transtorno alimentar</option>
+            </select>       
+        </div>
 
         <div class="col-12 mt-3 ">
-            <button type="submit" class="btn btn-primary">Agendar consulta</button>
+            <button type="submit" class="btn btn-success">Enviar meu cadastro</button>
         </div>
+
+        <div class="col-12">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                    <label class="form-check-label" for="flexCheckDefault">
+                        Concordo com os termos de uso <a href="#"> Termos e politica de uso</a>
+                    </label>
+        </div>
+
+        </div>
+
         </form>
 
 </div>
